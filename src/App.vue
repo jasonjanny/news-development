4bbed5a47ccc3f5b2f429e0c0e466499a4087ceb
janny <2431349655@qq.com>
2020-11-16 15:53:31 +0800

    引入字体图标

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
     <router-view />
+    <!-- 使用字体图标 -->
+    <span class="iconfont iconwode">123456</span>
   </div>
 </template>
 
 <style lang="less">
+/* 在 style 当中引入字体图标文件 */
+@import url("http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,5 +34,11 @@ ul,
 li,
 ol {
   list-style: none;
+}
+
+// 设置字体图标样式
+.iconwode {
+  font-size: 50px;
+  color: #bc9ee5;
 }
 </style>
