4170c21f6293d5ab5577a852ad48a5990277168b
janny <2431349655@qq.com>
2020-11-16 15:23:16 +0800

    子组件向父组件传值

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -1,12 +1,25 @@
 <template>
-  <!-- 子组件使用v-bind声明接收父组件数据 -->
-  <input type="text" :placeholder="tixing" />
+  <div>
+    <input type="text" :placeholder="tixing" v-model="value" />
+    <button @click="sendVal">点击发送</button>
+  </div>
 </template>
 
 <script>
 export default {
   // 获取外部参数
   props: ["tixing"],
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    sendVal() {
+      // 子组件通过$emit向父组件发送数据
+      this.$emit("getVal", this.value);
+    },
+  },
 };
 </script>
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,8 +3,11 @@
     <h1>这是一个登录页面</h1>
     <p>{{ msg }}</p>
     <!-- 用标签渲染子组件 -->
-    <!-- 父组件主动发出数据 -->
-    <MyInput tixing="请您输入内容"></MyInput>
+    <!-- 父组件监听事件 -->
+    <MyInput tixing="请您输入内容" @getVal="showVal"></MyInput>
+    <MyInput tixing="请您输入内容" @getVal="showVal"></MyInput>
+    <br />
+    <p>输入的内容为：{{ content }}</p>
   </div>
 </template>
 
@@ -15,11 +18,18 @@ export default {
   data() {
     return {
       msg: 1234,
+      content: "",
     };
   },
   // 注册子组件
   components: {
     MyInput,
+  },
+  methods: {
+    // 显示数据
+    showVal(val) {
+      this.content = val;
+    },
   },
 };
 </script>
