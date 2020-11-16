b0f9977f09274ec05c02cf01c019b78f729cbf96
janny <2431349655@qq.com>
2020-11-16 14:53:48 +0800

    实现单文件父组件内使用子组件

---
+++ src/components/MyInput.vue
@@ -0,0 +1,10 @@
+<template>
+  <input type="text" placeholder="请输入内容" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,15 +2,23 @@
   <div>
     <h1>这是一个登录页面</h1>
     <p>{{ msg }}</p>
+    <!-- 用标签渲染子组件 -->
+    <MyInput></MyInput>
   </div>
 </template>
 
 <script>
+// 引入子组件
+import MyInput from "../components/MyInput.vue";
 export default {
   data() {
     return {
       msg: 1234,
     };
+  },
+  // 注册子组件
+  components: {
+    MyInput,
   },
 };
 </script>
