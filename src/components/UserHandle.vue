800a42eb25517a25b2409ab72e2ac67382cf8eb3
janny <2431349655@qq.com>
2020-11-19 16:16:54 +0800

    实现了个人中心条形组件的封装和传参

---
+++ src/components/UserHandle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="userInfo">
+    <span>{{ list }}</span>
+    <p>
+      {{ about }}
+      <span class="iconfont iconjiantou1"></span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["list", "about"],
+};
+</script>
+
+<style lang="less" scoped>
+// 用户操作信息
+.userInfo {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 54/360 * 100vw;
+  margin: 0 16/360 * 100vw;
+  border-bottom: 1px solid #ccc;
+  p {
+    color: #949494dd;
+    font-size: 15/360 * 100vw;
+  }
+  span {
+    font-size: 16/360 * 100vw;
+  }
+}
+</style>
--- src/views/UserCenter.vue
+++ src/views/UserCenter.vue
@@ -4,7 +4,7 @@
     <div class="user">
       <!-- 用户头像 -->
       <div class="userImg">
-        <img src="../assets/logo.png" alt="" />
+        <img src="../assets/微信图片_20201104205241.jpg" alt="" />
       </div>
       <!-- 用户信息 -->
       <div class="userDetail">
@@ -22,18 +22,20 @@
     <!-- 装饰边框 -->
     <div class="border"></div>
     <!-- 用户操作信息 -->
-    <div class="userInfo">
-      <span>我的关注</span>
-      <p>
-        关注的用户
-        <span class="iconfont iconjiantou1"></span>
-      </p>
-    </div>
+    <UserHandle list="我的关注" about="关注的用户" />
+    <UserHandle list="我的跟帖" about="跟帖/回复" />
+    <UserHandle list="我的收藏" about="文章/视频" />
+    <UserHandle list="设置" />
   </div>
 </template>
 
 <script>
-export default {};
+import UserHandle from "../components/UserHandle";
+export default {
+  components: {
+    UserHandle,
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -66,6 +68,7 @@ export default {};
     margin-right: 110/360 * 100vw;
     .iconxingbienan {
       font-size: 18/360 * 100vw;
+      color: #6cf;
     }
     .nickname {
       margin-left: 8/360 * 100vw;
@@ -93,21 +96,5 @@ export default {};
 .border {
   height: 10/360 * 100vw;
   background-color: #ccc;
-}
-// 用户操作信息
-.userInfo {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 50/360 * 100vw;
-  margin: 0 16/360 * 100vw;
-  border-bottom: 1px solid #ccc;
-  p {
-    color: #949494dd;
-    font-size: 15/360 * 100vw;
-  }
-  span {
-    font-size: 16/360 * 100vw;
-  }
 }
 </style>
