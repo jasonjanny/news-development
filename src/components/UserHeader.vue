6031fbba8b3d296e7a592d8631f1d15c225c8ebc
janny <2431349655@qq.com>
2020-11-20 20:20:48 +0800

    封装了用户中心的头部组件

---
+++ src/components/UserHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- 头部 -->
+  <div class="editHeader">
+    <span class="iconfont iconjiantou" @click="back"></span>
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+  methods: {
+    back() {
+      this.$router.push("/usercenter");
+    },
+  },
+};
+</script>
+
+<style lang="less">
+// 头部
+.editHeader {
+  display: flex;
+  align-items: center;
+  height: 40/360 * 100vw;
+  padding-top: 10/360 * 100vw;
+  font-weight: 700;
+  .iconjiantou {
+    color: #888;
+    font-size: 16/360 * 100vw;
+  }
+  p {
+    flex: 1;
+    color: #333;
+    margin-right: 10/360 * 100vw;
+    font-size: 14/360 * 100vw;
+  }
+}
+</style>
