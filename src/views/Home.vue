86852c261b73434df4e437f683f8eef88558ca37
janny <2431349655@qq.com>
2020-11-24 18:18:25 +0800

    实现了分类栏目添加字段管理文章列表数据和获取当前分类文章列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,15 @@
         :title="category.name"
         v-for="category in categoryList"
         :key="category.id"
-        >内容 1</van-tab
       >
+        <!-- 文章列表 -->
+        <PostItem
+          :postData="list"
+          v-for="list in categoryList[activeCategoryIndex].postList"
+          :key="list.id"
+        />
+      </van-tab>
     </van-tabs>
-
-    <!-- 文章列表 -->
-    <PostItem :postData="list" v-for="list in postList" :key="list.id" />
   </div>
 </template>
 
@@ -24,7 +27,6 @@ import PostItem from "../components/IndexPostItem";
 export default {
   data() {
     return {
-      postList: [],
       categoryList: [],
       activeCategoryIndex: 0,
     };
@@ -39,18 +41,32 @@ export default {
     this.$axios({
       url: "/category",
     }).then((res) => {
-      console.log(res);
-      this.categoryList = res.data.data;
-      console.log(this.categoryList);
-    });
+      this.categoryList = res.data.data.map((category) => {
+        return {
+          // 展开语法
+          // 之前服务器获取到的栏目数据，全部保留
+          ...category,
+          // 在每个栏目里都添加一个数组
+          postList: [],
+        };
+      });
+      // console.log(this.categoryList);
 
-    // 获取文章列表
-    this.$axios({
-      url: "/post",
-    }).then((res) => {
-      // console.log(res);
-      this.postList = res.data.data;
-      console.log(this.postList);
+      // 获取当前默认处在激活状态的文章分类
+      const category = this.categoryList[this.activeCategoryIndex];
+
+      // 第一次获取文章列表
+      this.$axios({
+        url: "/post",
+        params: {
+          category: category.id,
+        },
+      }).then((res) => {
+        // console.log(res);
+        // 保存文章列表
+        this.categoryList[this.activeCategoryIndex].postList = res.data.data;
+        // console.log(this.categoryList);
+      });
     });
   },
 };
