e367b917946f1a7900cfcbe05c41442497307461
janny <2431349655@qq.com>
2020-11-24 15:41:35 +0800

    使用vant组件实现导航栏并且封装使用文章列表组件

---
+++ src/components/IndexPostItem.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <!-- 第1种文章模式 -->
+    <div
+      class="articlePattern1"
+      v-if="
+        postData.type === 1 &&
+        postData.cover.length > 0 &&
+        postData.cover.length < 3
+      "
+    >
+      <div class="left">
+        <p>{{ postData.title }}</p>
+        <p class="from">
+          <span
+            >{{ postData.user.nickname }}&nbsp;&nbsp;{{
+              postData.comment_length
+            }}跟帖</span
+          >
+        </p>
+      </div>
+      <div class="right">
+        <img :src="fixImgUrl(postData.cover[0].url)" alt="" />
+      </div>
+    </div>
+
+    <!-- 第2种文章模式 -->
+    <div
+      class="articlePattern2"
+      v-if="postData.type === 1 && postData.cover.length >= 3"
+    >
+      <p>
+        {{ postData.title }}
+      </p>
+      <div class="picture">
+        <img :src="fixImgUrl(postData.cover[0].url)" alt="" />
+        <img :src="fixImgUrl(postData.cover[1].url)" alt="" />
+        <img :src="fixImgUrl(postData.cover[2].url)" alt="" />
+      </div>
+      <p class="from">
+        <span
+          >{{ postData.user.nickname }}&nbsp;&nbsp;{{
+            postData.comment_length
+          }}跟帖</span
+        >
+      </p>
+    </div>
+
+    <!-- 第3种文章模式 -->
+    <div v-if="postData.type === 2 && postData.cover.length > 0">
+      <div class="articlePattern3">
+        <p>{{ postData.title }}</p>
+        <div class="center">
+          <img class="cover" :src="fixImgUrl()" alt="" />
+          <span class="iconfont iconshipin"></span>
+        </div>
+        <p class="from">
+          <span
+            >{{ postData.user.nickname }}&nbsp;&nbsp;{{
+              postData.comment_length
+            }}跟帖</span
+          >
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["postData"],
+  methods: {
+    fixImgUrl(oldUrl) {
+      if (oldUrl.indexOf("http") > -1) {
+        return oldUrl;
+      } else {
+        return this.$axiosaxios.defaults.baseURL + oldUrl;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.from {
+  margin: 12/360 * 100vw 0;
+  font-size: 13/360 * 100vw;
+  color: #868686;
+}
+
+.articlePattern1 {
+  display: flex;
+  padding: 18/360 * 100vw 10/360 * 100vw 0;
+  border-bottom: 1px solid #ccc;
+  .left {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .right {
+    width: 121/360 * 100vw;
+    height: 75/360 * 100vw;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+
+.articlePattern2 {
+  padding: 18/360 * 100vw 10/360 * 100vw 0;
+  border-bottom: 1px solid #ccc;
+  .picture {
+    display: flex;
+    height: 74/360 * 100vw;
+    margin-top: 8/360 * 100vw;
+    img {
+      width: 33%;
+      height: 100%;
+      margin-right: 2/360 * 100vw;
+      object-fit: cover;
+    }
+  }
+}
+
+.articlePattern3 {
+  padding: 18/360 * 100vw 10/360 * 100vw 0;
+  border-bottom: 1px solid #ccc;
+  .center {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 340/360 * 100vw;
+    height: 170/360 * 100vw;
+    background-color: #6cf;
+    .cover {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+    .iconshipin {
+      position: absolute;
+      color: #fff;
+      font-size: 46/360 * 100vw;
+      background-color: rgba(0, 0, 0, 0.205);
+      border-radius: 50%;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,97 +3,55 @@
     <!-- 头部 -->
     <Header />
 
-    <!-- 第1种文章模式 -->
-    <div class="articlePattern1">
-      <div class="left">
-        <p>如果希望在封装的开闭组件内使用</p>
-        <p>一二三四五六七</p>
-        <p class="from">
-          <span>火星时报&nbsp;&nbsp;52跟帖</span>
-        </p>
-      </div>
-      <div class="right">
-        <img src="../assets/1.jpg" alt="" />
-      </div>
-    </div>
+    <!-- 导航栏 -->
+    <van-tabs v-model="activeCategoryIndex" background="#f2f2f2">
+      <van-tab
+        :title="category.name"
+        v-for="category in categoryList"
+        :key="category.id"
+        >内容 1</van-tab
+      >
+    </van-tabs>
 
-    <!-- 第2种文章模式 -->
-    <div class="articlePattern2">
-      <p>
-        如果想要在父组件影响子组件样式，只需在父组件CSS中在样式的前面使用
-        /deep/即可
-      </p>
-      <div class="picture">
-        <img src="../assets/1.jpg" alt="" />
-        <img src="../assets/1.jpg" alt="" />
-        <img src="../assets/1.jpg" alt="" />
-      </div>
-      <p class="from">
-        <span>火星时报&nbsp;&nbsp;52跟帖</span>
-      </p>
-    </div>
-
-    <!-- 第3种文章模式 -->
-    <div class="articlePattern3">
-      <p>实现mvvm主要包含两个方面，数据变化更新视图，视图变化更新数据</p>
-      <div class="radio">
-        <img class="circle" src="../assets/2.png" alt="" />
-        <img class="switch" src="../assets/switch.png" alt="" />
-      </div>
-      <p class="from">
-        <span>火星时报&nbsp;&nbsp;52跟帖</span>
-      </p>
-    </div>
-
-    <!-- 第1种文章模式 -->
-    <div class="articlePattern1">
-      <div class="left">
-        <p>如果希望在封装的开闭组件内使用</p>
-        <p>一二三四五六七</p>
-        <p class="from">
-          <span>火星时报&nbsp;&nbsp;52跟帖</span>
-        </p>
-      </div>
-      <div class="right">
-        <img src="../assets/1.jpg" alt="" />
-      </div>
-    </div>
-
-    <!-- 第2种文章模式 -->
-    <div class="articlePattern2">
-      <p>
-        如果想要在父组件影响子组件样式，只需在父组件CSS中在样式的前面使用
-        /deep/即可
-      </p>
-      <div class="picture">
-        <img src="../assets/1.jpg" alt="" />
-        <img src="../assets/1.jpg" alt="" />
-        <img src="../assets/1.jpg" alt="" />
-      </div>
-      <p class="from">
-        <span>火星时报&nbsp;&nbsp;52跟帖</span>
-      </p>
-    </div>
-
-    <!-- 第3种文章模式 -->
-    <div class="articlePattern3">
-      <p>实现mvvm主要包含两个方面，数据变化更新视图，视图变化更新数据</p>
-      <div class="radio">
-        <img class="circle" src="../assets/2.png" alt="" />
-        <img class="switch" src="../assets/switch.png" alt="" />
-      </div>
-      <p class="from">
-        <span>火星时报&nbsp;&nbsp;52跟帖</span>
-      </p>
-    </div>
+    <!-- 文章列表 -->
+    <PostItem :postData="list" v-for="list in postList" :key="list.id" />
   </div>
 </template>
 
 <script>
 import Header from "../components/IndexHeader";
+import PostItem from "../components/IndexPostItem";
 export default {
+  data() {
+    return {
+      postList: [],
+      categoryList: [],
+      activeCategoryIndex: 0,
+    };
+  },
   components: {
     Header,
+    PostItem,
+  },
+
+  created() {
+    // 获取栏目列表
+    this.$axios({
+      url: "/category",
+    }).then((res) => {
+      console.log(res);
+      this.categoryList = res.data.data;
+      console.log(this.categoryList);
+    });
+
+    // 获取文章列表
+    this.$axios({
+      url: "/post",
+    }).then((res) => {
+      // console.log(res);
+      this.postList = res.data.data;
+      console.log(this.postList);
+    });
   },
 };
 </script>
@@ -101,67 +59,5 @@ export default {
 <style lang="less" scoped>
 .container {
   min-height: 100vh;
-  background-color: #fff;
-
-  .from {
-    margin: 12/360 * 100vw 0;
-    font-size: 13/360 * 100vw;
-    color: #868686;
-  }
-
-  .articlePattern1 {
-    display: flex;
-    padding: 18/360 * 100vw 10/360 * 100vw 0;
-    border-bottom: 1px solid #ccc;
-    .left {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-    }
-    .right {
-      width: 121/360 * 100vw;
-      height: 75/360 * 100vw;
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-  }
-
-  .articlePattern2 {
-    padding: 18/360 * 100vw 10/360 * 100vw 0;
-    border-bottom: 1px solid #ccc;
-    .picture {
-      display: flex;
-      height: 74/360 * 100vw;
-      margin-top: 8/360 * 100vw;
-      img {
-        flex: 1;
-        height: 100%;
-        margin-right: 2/360 * 100vw;
-      }
-    }
-  }
-
-  .articlePattern3 {
-    padding: 18/360 * 100vw 10/360 * 100vw 0;
-    border-bottom: 1px solid #ccc;
-    .radio {
-      position: relative;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 340/360 * 100vw;
-      height: 170/360 * 100vw;
-      .circle {
-        opacity: 0.2;
-      }
-      .switch {
-        position: absolute;
-        width: 55/360 * 100vw;
-      }
-    }
-  }
 }
 </style>
