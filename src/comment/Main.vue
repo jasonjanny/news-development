e8af85ac31dfb888c77b831f213893dc2ea28ec9
janny <2431349655@qq.com>
2020-11-29 15:51:34 +0800

    实现楼层回复递归获取数据并渲染

---
+++ src/comment/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="mainContainer" v-if="mainlist.parent">
+      <Parent :parentlist="mainlist.parent" />
+    </div>
+
+    <div class="main"></div>
+  </div>
+</template>
+
+<script>
+import Parent from "../comment/Parent";
+export default {
+  props: ["mainlist"],
+  components: {
+    Parent,
+  },
+};
+</script>
+<style lang="less" scoped>
+.mainContainer {
+  margin: 15/360 * 100vw;
+  padding: 5/360 * 100vw 15/360 * 100vw 10/360 * 100vw;
+  border: 1px solid #ccc;
+}
+.main {
+  margin: 15/360 * 100vw;
+}
+</style>
---
+++ src/comment/Parent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="parentContainer">
+    <!-- 先递归到最深层再一层一层显示数据 -->
+    <Parent v-if="parentlist.parent" :parentlist="parentlist.parent" />
+    <div class="content">
+      <div class="userInfo">
+        <div class="number">1</div>
+        <div class="nickname">{{ parentlist.user.id }}</div>
+        <div class="date">2小时前</div>
+        <div class="reply">回复</div>
+      </div>
+      <div class="userContent">{{ parentlist.content }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Parent",
+  props: ["parentlist"],
+};
+</script>
+
+<style lang="less" scoped>
+.parentContainer {
+  .content {
+    border-bottom: 1px solid #ccc;
+    margin-top: 10/360 * 100vw;
+    .userInfo {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10/360 * 100vw;
+      .number {
+        padding-right: 10/360 * 100vw;
+      }
+      .nickname {
+        padding-right: 10/360 * 100vw;
+      }
+      .date {
+        flex: 1;
+        font-size: 12/360 * 100vw;
+        color: #888;
+      }
+      .reply {
+        font-size: 12/360 * 100vw;
+        color: #888;
+      }
+    }
+    .userContent {
+      font-size: 14/360 * 100vw;
+      padding-bottom: 10/360 * 100vw;
+    }
+  }
+}
+</style>
